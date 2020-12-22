ac03ad9bb7fca52768a288c72dbf6170930033b3
fl0ffy <bernat.johnathan@gmail.com>
2020-12-22 15:15:44 -0500

    modified saying hello to meet the constraint and first challenge.

--- src/saying_hello.py
+++ src/saying_hello.py
@@ -1,3 +1,17 @@
+# 01 Saying Hello
+#     create a program that prompts for your name and prints a greeting using your name.
+# Example Output
+# ​ 	What is your name? Brian
+# ​ 	Hello, Brian, nice to meet you!
+# Constraint
+#     Keep the input, string concatenation, and output separate.
+# Challenges
+#
+#     Write a new version of the program without using any variables.
+#
+#     Write a version of the program that displays different greetings for different people.
+#     This would be a good challenge to try after you’ve completed the exercises in Chapter 4,
+#     ​Making Decisions​ and Chapter 7, ​Data Structures​.
 
 
 def get_input() -> str:
@@ -8,14 +22,13 @@ def generate_greeting(name: str) -> str:
     return f'Hello, {name}, nice to meet you!'
 
 
-def  output(s: str) -> None:
+def output(s: str) -> None:
     print(s)
     return
 
 
 def saying_hello() -> None:
-    name = get_input()
-    output(generate_greeting(name))
+    output(generate_greeting(get_input()))
 
 
 if __name__ == '__main__':
